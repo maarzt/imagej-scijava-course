e1956d0cf973e88e48b9034215c05cad9eb64e46
Matthias Arzt <arzt@mpi-cbg.de>
2019-06-25 14:03:07 +0200

    Add tiny jython example

---
+++ src/main/java/de/mpicbg/learnathon/course/scijava/services/Example4.py
@@ -0,0 +1,8 @@
+# @LogService logService
+# @UIService uiService
+
+# Please open and run this Jython script using the FIJI script editor.
+# The "@Service service" syntax used in the first lines, allows to use services in scripts.
+
+logService.info("Hello World")
+uiService.show("Apple")
