59b8eaaf1771ce0ce2f51687304b60a924ffa57f
Matthias Arzt <arzt@mpi-cbg.de>
2019-06-26 23:07:02 +0200

    Remove AnimalService

--- src/main/java/de/mpicbg/learnathon/course/scijava/exercise2_animal_service/Animal.java
+++
@@ -1,8 +0,0 @@
-package de.mpicbg.learnathon.course.scijava.exercise2_animal_service;
-
-import org.scijava.plugin.SciJavaPlugin;
-
-public interface Animal extends SciJavaPlugin {
-
-	boolean isCat();
-}
