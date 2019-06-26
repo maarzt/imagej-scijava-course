fddadee6c2c6af87067771a2ebf156ad6b63399f
Matthias Arzt <arzt@mpi-cbg.de>
2019-06-26 23:12:14 +0200

    Solution to the color display exercise

---
+++ src/main/java/de/mpicbg/learnathon/course/scijava/exercise4_implement_color_display/ColorDisplay.java
@@ -0,0 +1,62 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2016 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package de.mpicbg.learnathon.course.scijava.exercise4_implement_color_display;
+
+import de.mpicbg.learnathon.course.bird_display.Bird;
+import org.scijava.display.AbstractDisplay;
+import org.scijava.display.Display;
+import org.scijava.plugin.Plugin;
+
+import java.awt.*;
+
+/**
+ * Very simple UI independent class, that's required to display the {@link Bird}.
+ * This class can potentially hold information about how to display the bird,
+ * that can be anything like: font size, color, display mode. This simple example
+ * doesn't use any of these possibilities.
+ */
+@Plugin(type = Display.class) public class ColorDisplay
+		extends AbstractDisplay< Color >
+{
+
+	public ColorDisplay() {
+		super(Color.class);
+	}
+
+	// --- Optionally it's possible to filter:
+
+	//	@Override
+	//	public boolean canDisplay( Object o )
+	//	{
+	//		return o instanceof Bird;
+	//	}
+}
